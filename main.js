ccce8fda2dde3002f91fd5e64a1cdb87362b1424
micharosa211 <112971548+micharosa211@users.noreply.github.com>
2022-09-23 22:48:56 +0200

    poprawka

---
+++ main.js
@@ -0,0 +1,12 @@
+let par1 = "Uwielbiam JavaScript";
+let par2 = "Jestem świetnym programistą";
+
+function z(par1, par2) {
+    if (par1.length > par2.length) {
+        console.log(par1);
+} else {
+    console.log(par2);
+}
+}
+z(par1, par2);
+
